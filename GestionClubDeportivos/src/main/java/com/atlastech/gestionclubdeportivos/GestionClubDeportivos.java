d4b25e2938ab162e285e3ef8a9d039423efde5f1
Loreni Troncoso <lorenic1803@gmail.com>
2025-11-25 11:38:52 -0400

    conexion

--- GestionClubDeportivos/src/main/java/com/atlastech/gestionclubdeportivos/GestionClubDeportivos.java
+++ GestionClubDeportivos/src/main/java/com/atlastech/gestionclubdeportivos/GestionClubDeportivos.java
@@ -8,9 +8,20 @@ package com.atlastech.gestionclubdeportivos;
  *
  * @author bryanpeguerocamilo
  */
+import com.atlastech.gestionclubdeportivos.databases.Databases;
+import java.sql.Connection;
 public class GestionClubDeportivos {
 
     public static void main(String[] args) {
         System.out.println("Hello Atlas Tech!");
+        Connection cn = Databases.getConection();
+
+
+        // Verificar conexión
+        if (cn != null) {
+            System.out.println("Connected from main successfully :)");
+        } else {
+            System.out.println("Connection failed from main :(");
+        }
     }
 }
