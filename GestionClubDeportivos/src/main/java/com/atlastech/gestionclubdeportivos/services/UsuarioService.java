44e882523113360a4acab88a8be8ce0b5b665305
edwindelossantos08 <20251032@itla.edu.do>
2025-12-01 18:24:53 -0400

    UsuarioDAO + UsuarioService!

---
+++ GestionClubDeportivos/src/main/java/com/atlastech/gestionclubdeportivos/services/UsuarioService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.atlastech.gestionclubdeportivos.services;
+
+/**
+ *
+ * @author bryanpeguerocamilo
+ */
+public class UsuarioService {
+    
+}
